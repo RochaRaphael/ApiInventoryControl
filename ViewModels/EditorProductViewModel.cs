3a927b79915f1d1caa4b9a49552d326f5fd3a8f1
Raphael Rocha <rocharaphael0911@outlook.com>
2022-12-19 16:30:09 -0300

    EditorViewModel created

---
+++ ViewModels/EditorProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ApiInventoryControl.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiInventoryControl.ViewModels
+{
+    public class EditorProductViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "This field must contain between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        public Category Category { get; set; }
+    }
+}

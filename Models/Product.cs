23de24fced83325a9f35fdc3f8085ab70a24f046
Raphael Rocha <rocharaphael0911@outlook.com>
2022-12-19 14:06:56 -0300

    Product Model created

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiInventoryControl.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}

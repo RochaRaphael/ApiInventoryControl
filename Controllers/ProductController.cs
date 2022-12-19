7f6bfe473b4b49b34c368e8725cadb5ba69e7b8f
Raphael Rocha <rocharaphael0911@outlook.com>
2022-12-19 16:20:41 -0300

    Post method added

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using ApiInventoryControl.Data;
+using ApiInventoryControl.Extensions;
 using ApiInventoryControl.Models;
 using ApiInventoryControl.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +10,7 @@ namespace ApiInventoryControl.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        [HttpGet("v1/Product/{name:string}")]
+        [HttpGet("v1/product/{name:string}")]
         public async Task<IActionResult> GetAsync(
             [FromRoute] string name,
             [FromServices] InventoryDataContext context
@@ -26,6 +27,40 @@ namespace ApiInventoryControl.Controllers
             catch
             {
                 return StatusCode(500, "05X10 - Internal server failure");
+            }
+        }
+
+
+
+        [HttpPost("v1/product")]
+        public async Task<IActionResult> PostAsync(
+            [FromBody] Product model,
+            [FromServices] InventoryDataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
+            try
+            {
+                var product = new Product
+                {
+                    Name = model.Name,
+                    Price = model.Price,
+                    Quantity = model.Quantity,
+                    Category = model.Category
+                };
+                await context.Products.AddAsync(product);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{product.Id}", new ResultViewModel<Product>(product));
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new ResultViewModel<Category>("05XE9 - Unable to add product"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Category>("05X10 - Internal server failure"));
             }
         }
     }
